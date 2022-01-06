b879d4fdfb93879557f9692c10143ccbba22802a
Hsin-Ni Yen <yensydney@gmail.com>
2022-01-05 22:23:47 -0800

    I removed the error message at the bottom by not returning -1 and continuing the loop instead. I specified which record the CA Amboy could not retrieve. Then I created the json file for CA Amboy with the successfully retrieved records.

--- newMain.py
+++ newMain.py
@@ -27,13 +27,16 @@ def parseSFWithKey( sf, phrase, word, loc, fileName ):
     #     print(oid, coord)
     #     print()
     # sys.exit()
-
+    print(len(records))
     # turning each record into a dictionary and adding it to a list
+    print
     for i in range(len(records)):
         currentRecord = sf.record(i)
+
         if currentRecord == None:
-            print( "ERROR: Failed to retrieve record from", fileName )
-            return -1
+            print
+            print( "ERROR: Failed to retrieve record from record", i, "in", fileName )
+            continue
 
         # print(currentRecord)
         dic = currentRecord.as_dict()
